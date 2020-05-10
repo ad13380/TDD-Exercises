b8764ee3cdbd056eea1b82be772743e0ffe2c467
Anthony <a.a.donovan@outlook.com>
2020-05-10 13:19:31 +0100

    Adds to_camel_case function and passes ArgumentError tests

---
+++ Convert_camelcase/spec/camel_spec.rb
@@ -0,0 +1,16 @@
+require 'camel'
+
+describe '#to_camel_case' do
+
+
+  it 'throws an ArgumentError when given more than 1 argument' do
+    camel_object = lambda { to_camel_case("a","b") }
+    expect(camel_object).to raise_error(ArgumentError)
+  end
+
+  it 'throws return an ArgumentError when given 0 arguments' do
+    camel_object = lambda { to_camel_case() }
+    expect(camel_object).to raise_error(ArgumentError)
+  end
+
+end
